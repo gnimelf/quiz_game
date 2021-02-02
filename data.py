9296e36e9faf1b37d7787ecfd23d96b6b623e68f
gnimelf <gnimelf@users.noreply.github.com>
2021-02-02 17:51:22 -0500

    added file via upload

---
+++ data.py
@@ -0,0 +1,83 @@
+question_data = [
+    {
+        "category": "Entertainment: Musicals & Theatres",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the play Oedipus Rex, Oedipus kills his father due to jealousy in loving his mother.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Eurobeat is primarily an Italian genre of music.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": 'Norwegian producer Kygo released a remix of the song "Sexual Healing" by Marvin Gaye.',
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The country song  &ldquo;A Boy Named Sue&rdquo; was written by Shel Silverstein.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+     },
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Type 1 diabetes is a result of the liver working improperly.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Donkey Kong was originally set to be a Popeye arcade game.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": 'The 2005 video game "Call of Duty 2: Big Red One" is not available on PC.',
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]}
+    ,
+    {
+        "category": "Entertainment: Board Games",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The board game Go has more possible legal positions than the number of atoms "
+                    "in the visible universe.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": 'In "Star Trek: The Next Generation", Data is the only android in existence.',
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Cartoon & Animations",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Nutcracker Suite was one of the musical pieces featured in Disney&#039;s 1940&#039;s film Fantasia.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
